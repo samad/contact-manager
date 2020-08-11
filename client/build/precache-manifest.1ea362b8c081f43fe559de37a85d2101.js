9b5fd4501026a1906bfd417d67d235deab418071
Abdul Samad <Samk8778@yahoo.com>
2020-08-11 14:36:48 +0530

    :rocket: Deploying navbar responsive toggle

---
+++ client/build/precache-manifest.1ea362b8c081f43fe559de37a85d2101.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2795c3df1ff4b07a8d412d7bdeeb0c8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c81efd90f5ab8bd96fbd",
+    "url": "/static/css/main.8563880e.chunk.css"
+  },
+  {
+    "revision": "0028991b0ddb597b1aa8",
+    "url": "/static/js/2.6bf4dc8d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6bf4dc8d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c81efd90f5ab8bd96fbd",
+    "url": "/static/js/main.295432a6.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
