15624300ddcb2d38ffde9a024f38ff85068b6a4d
Abdul Samad <Samk8778@yahoo.com>
2021-05-30 17:35:46 +0530

    :rotating_light: Fixed eslint linting warning

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -1,91 +1,95 @@
 import {
-	ADD_CONTACT,
-	DELETE_CONTEXT,
-	SET_CURRENT,
-	CLEAR_CURRENT,
-	UPDATE_CONTACT,
-	FILTER_CONTACT,
-	CLEAR_FILTER,
-	CONTACT_ERROR,
-	GET_CONTACTS,
-	CLEAR_CONTACTS,
-} from '../types';
+  ADD_CONTACT,
+  DELETE_CONTEXT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  UPDATE_CONTACT,
+  FILTER_CONTACT,
+  CLEAR_FILTER,
+  CONTACT_ERROR,
+  GET_CONTACTS,
+  CLEAR_CONTACTS,
+} from "../types";
 
-export default (state, action) => {
-	switch (action.type) {
-		case GET_CONTACTS:
-			return {
-				...state,
-				contacts: action.payload,
-				loading: false,
-			};
+const contactReducer = (state, action) => {
+  switch (action.type) {
+    case GET_CONTACTS:
+      return {
+        ...state,
+        contacts: action.payload,
+        loading: false,
+      };
 
-		case ADD_CONTACT:
-			return {
-				...state,
-				contacts: [...state.contacts, action.payload],
-				loading: false,
-			};
+    case ADD_CONTACT:
+      return {
+        ...state,
+        contacts: [...state.contacts, action.payload],
+        loading: false,
+      };
 
-		case UPDATE_CONTACT:
-			return {
-				...state,
-				contacts: state.contacts.map((contact) =>
-					contact._id === action.payload._id ? action.payload : contact,
-				),
-				loading: false,
-			};
+    case UPDATE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.map((contact) =>
+          contact._id === action.payload._id ? action.payload : contact
+        ),
+        loading: false,
+      };
 
-		case DELETE_CONTEXT:
-			return {
-				...state,
-				contacts: state.contacts.filter((contact) => contact._id !== action.payload),
-				loading: false,
-			};
+    case DELETE_CONTEXT:
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          (contact) => contact._id !== action.payload
+        ),
+        loading: false,
+      };
 
-		case CLEAR_CONTACTS:
-			return {
-				...state,
-				contacts: null,
-				filtered: null,
-				error: null,
-				current: null,
-			};
+    case CLEAR_CONTACTS:
+      return {
+        ...state,
+        contacts: null,
+        filtered: null,
+        error: null,
+        current: null,
+      };
 
-		case SET_CURRENT:
-			return {
-				...state,
-				current: action.payload,
-			};
+    case SET_CURRENT:
+      return {
+        ...state,
+        current: action.payload,
+      };
 
-		case CLEAR_CURRENT:
-			return {
-				...state,
-				current: null,
-			};
+    case CLEAR_CURRENT:
+      return {
+        ...state,
+        current: null,
+      };
 
-		case FILTER_CONTACT:
-			return {
-				...state,
-				filtered: state.contacts.filter((contact) => {
-					const regex = new RegExp(`${action.payload}`, 'gi');
-					return contact.name.match(regex) || contact.email.match(regex);
-				}),
-			};
+    case FILTER_CONTACT:
+      return {
+        ...state,
+        filtered: state.contacts.filter((contact) => {
+          const regex = new RegExp(`${action.payload}`, "gi");
+          return contact.name.match(regex) || contact.email.match(regex);
+        }),
+      };
 
-		case CLEAR_FILTER:
-			return {
-				...state,
-				filtered: null,
-			};
+    case CLEAR_FILTER:
+      return {
+        ...state,
+        filtered: null,
+      };
 
-		case CONTACT_ERROR:
-			return {
-				...state,
-				error: action.payload,
-			};
+    case CONTACT_ERROR:
+      return {
+        ...state,
+        error: action.payload,
+      };
 
-		default:
-			return { ...state };
-	}
+    default:
+      return { ...state };
+  }
 };
+
+export default contactReducer;
